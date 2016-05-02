70d7a5d65093990de86e7def0cca78d90c044535
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-05-02 11:10:51 -0500

    refactor code for Discrete-Finite-State, Discrete-Infinite-Time, Discounted-Value problems

--- dy/DiscrFinState_DiscrInfinTime_DiscValue.py
+++ dy/DiscrFinState_DiscrInfinTime_DiscValue.py
@@ -13,7 +13,7 @@ class Prob:
             bellman_op=lambda terminal_expected_value: array([[0.]])):
         self.nb_states = nb_states
         self.state_transition_prob_matrix_func = state_transition_prob_matrix_func
-        self.expected_values_per_stage_func=expected_values_per_stage_func
+        self.expected_values_per_stage_func = expected_values_per_stage_func
         self.discount_factor = discount_factor
         self.bellman_op = bellman_op
 
@@ -31,7 +31,13 @@ class Prob:
             prev_values = curr_values.copy()
             curr_values = self.bellman_op(terminal_expected_values=prev_values, return_policy=False)
         print('done!')
-        return self.bellman_op(terminal_expected_values=curr_values, return_policy=True)
+        return tuple(self.bellman_op(terminal_expected_values=curr_values, return_policy=True))
+
+    def relative_value_iteration(self, rtol=1e-5, atol=1e-8):
+        pass
+
+    def q_factor_value_iteration(self):
+        pass
 
     def policy_iteration(self, init_policy=None):
         if init_policy:
@@ -40,20 +46,23 @@ class Prob:
             curr_policy = self.nb_states * (0,)
         prev_policy = None
         identity_matrix = identity(self.nb_states)
-        matrix_inverses = {}
+        cached_matrix_inverses = {}
         i = 0
         print('Running Policy Iteration #', end='')
         while (prev_policy is None) or (curr_policy != prev_policy):
             i += 1
             print(i, end=', ')
             prev_policy = curr_policy
-            if prev_policy in matrix_inverses:
-                matrix_inverse = matrix_inverses[prev_policy]
+            if prev_policy in cached_matrix_inverses:
+                matrix_inverse = cached_matrix_inverses[prev_policy]
             else:
                 matrix_inverse = \
                     inv(identity_matrix - self.discount_factor * self.state_transition_prob_matrix_func(prev_policy))
-                matrix_inverses[prev_policy] = matrix_inverse
+                cached_matrix_inverses[prev_policy] = matrix_inverse
             values = matrix_inverse.dot(self.expected_values_per_stage_func(prev_policy))
             curr_policy = tuple(self.bellman_op(terminal_expected_values=values, return_policy=True))
         print('done!')
         return curr_policy
+
+    def q_factor_policy_iteration(self):
+        pass
